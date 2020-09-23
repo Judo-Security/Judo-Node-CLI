1614749e908b3422a9d66746afbd9a03d2ace04a
tapanchudasama <tapan.chudasama@ajmerainfotech.com>
2020-09-23 17:15:55 +0530

    JSS-641 File not being decrypted properly bug fixed

--- src/commands/read.js
+++ src/commands/read.js
@@ -4,9 +4,29 @@ const aes256 = require('../aes256gcm');
 const logger = require('../utils/logger');
 const judo = require('../judofile');
 const getShards = require('../services/getShards');
-const readOutputFile = require('../utils/helper');
 
-function read({ storageKey, inputFile, verbose }) {
+function readOutputFile(secretType, outputFile, save) {
+  return new Promise((resolve, reject) => {
+    const path = save ? save + '/' + outputFile : outputFile;
+    if (secretType === 2 && outputFile && outputFile.length > 0) {
+      return fs.access(path, fs.constants.F_OK, (err) => {
+        if (err) {
+          // file doesn't exist
+          resolve();
+        } else {
+          if (save) {
+            resolve();
+          } else {
+            reject('File cannot be displayed on STDOUT. Specify location to save the Judo file.');
+          }
+        }
+      });
+    } else {
+      resolve();
+    }
+  });
+}
+function read({ storageKey, inputFile, save, verbose }) {
   const startTime = new Date();
   logger.log(`Reading Judo file: ${inputFile}`, logger.MESSAGE_TYPE.WARN, verbose);
 
@@ -20,7 +40,7 @@ function read({ storageKey, inputFile, verbose }) {
   const outputFile = judoFile.filename;
   const secretType = judoFile.type;
   // if we have an outputfile defined, let's see if it already exists.
-  readOutputFile({secretType, outputFile}).then(fileName => {
+  readOutputFile(secretType, outputFile, save).then(() => {
     // download the shards
     logger.log('Downloading shards.', logger.MESSAGE_TYPE.INFO, verbose);
     getShards(judoFile.secretId, judoFile.shardUrls, storageKey).then((results) => {
@@ -35,7 +55,19 @@ function read({ storageKey, inputFile, verbose }) {
         const decryptedData = aes256.decrypt(Buffer.from(decryptedDek, 'base64'), judoFile.data);
         const decryptedText = Buffer.from(decryptedData, 'base64');
 
-        logger.log(decryptedText.toString('utf8'), logger.MESSAGE_TYPE.INFO, true);
+        if (secretType === 2 && outputFile && outputFile.length > 0) {
+          const path = save ? save + '/' + outputFile : outputFile;
+          // write file
+          fs.writeFile(path, decryptedText, function (err) {
+            if (err) {
+              logger.log(err.message, logger.MESSAGE_TYPE.ERROR, verbose);
+            }
+            logger.log(`Output written to: ${path}`, logger.MESSAGE_TYPE.LOG, verbose);
+          });
+        } else {
+          // log the decrypted text
+          logger.log(decryptedText.toString('utf8'), logger.MESSAGE_TYPE.INFO, true);
+        }
         // log the time taken
         const timeTaken = new Date() - startTime;
         logger.log(`Time taken: ${timeTaken}ms`, logger.MESSAGE_TYPE.INFO, verbose)
